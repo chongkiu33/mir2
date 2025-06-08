8b9084c613ca263671dfeeaf0f515cdacbbd0c73
chongkiu333 <xingshanhe33@gmail.com>
2025-06-09 01:35:46 +0800

    change

--- src/app/api/draft-mode/enable/route.ts
+++ src/app/api/draft-mode/enable/route.ts
@@ -5,7 +5,8 @@
 
 import { defineEnableDraftMode } from "next-sanity/draft-mode";
 import { client } from "@/sanity/lib/client";
-import { token } from "@/sanity/lib/token"
+import { token } from "@/sanity/lib/token";
+import { cookies } from 'next/headers';
 
 export const { GET } = defineEnableDraftMode({
   client: client.withConfig({ token }),
